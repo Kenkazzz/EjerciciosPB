f8a61b3b827dde598e1fb0dbeef7138fb9bcfc34
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:23:23 -0500

    Ejercicio60.java

--- 60-69/Ejercicio60.java
+++ 60-69/Ejercicio60.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class Ejercicio60 {
-    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numero;
+        int contador = 0;
+        
+        System.out.println("Ingrese números (ingrese 0 para terminar):");
+        
+        do {
+            numero = entrada.nextInt();
+            if(numero > 0) {
+                contador++;
+            }
+        } while(numero != 0);
+        
+        System.out.println("Cantidad de números mayores que cero leídos: " + contador);
+        
+        sc.close();
+    }
 }
