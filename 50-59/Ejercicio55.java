b29f85a8fe0e8d86c18b32ba30d7ef0a32eec7b9
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:20:07 -0500

    Ejercicio55.java

--- 50-59/Ejercicio55.java
+++ 50-59/Ejercicio55.java
@@ -1,3 +1,22 @@
-public  class Ejercicio55 {
-    
+import java.util.*;
+public class Ejercicio55 {
+    public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite un numero");
+        int numero = entrada.nextInt();
+
+         int suma=0;
+
+         for(int i=1;i<=numero;i++){
+            int calculo = numero%i;
+            if (calculo==0) {
+                suma++;
+            }
+         }
+         if (suma<=2) {
+            System.out.println("Su numero es primo");
+         }else{
+            System.out.println("Su numero no es primo");
+         }
+    }
 }
