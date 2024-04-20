7d95a7eaf178acaffce0abbe8b832ef8c1b2608a
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:09:55 -0500

    Ejercicio46.java

--- 40-49/Ejercicio46.java
+++ 40-49/Ejercicio46.java
@@ -1,3 +1,12 @@
+import java.util.*;
 public class Ejercicio46 {
-    
+    public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digita la cantidad de millas marinas");
+
+        int millas = entrada.nextInt();
+        int metros = 1852*millas;
+        
+        System.out.println("La cantidad de metros de "+millas+" millas marinas es de "+metros);
+    }
 }
