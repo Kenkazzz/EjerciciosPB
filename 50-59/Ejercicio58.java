9645df32d8028d4d774266dd3cde41220d226d70
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:21:53 -0500

    Ejercicio58.java

--- 50-59/Ejercicio58.java
+++ 50-59/Ejercicio58.java
@@ -1,3 +1,15 @@
+import java.util.*;
 public class Ejercicio58 {
-    
+    public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite las horas trabajadas");
+        int horas = entrada.nextInt();
+
+        System.out.println("Digite el pago por hora");
+        double pago = entrada.nextDouble();
+
+        double salario = pago*horas;
+
+        System.out.println("El salario es de "+salario+" pesos");
+    }
 }
