fb8e806018bb4e5e41d13cb8e31c0f610fa9fefb
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:24:49 -0500

    Ejercicio62.java

--- 60-69/Ejercicio62.java
+++ 60-69/Ejercicio62.java
@@ -1,3 +1,36 @@
+import java.util.*;
+
 public class Ejercicio62 {
-    
+     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        int sumaTempe = 0;
+        int temperaturaM = 0;
+        int temperaturaMini = 0;
+        int tempe = 4;
+
+        for(int i = 1; i <= 6; i++) {
+            System.out.println("Ingrese la temperatura exterior de las "+tempe+" horas: ");
+            int temperatura = entrada.nextInt();
+            
+            sumaTempe += temperatura;
+
+            tempe += 4;
+            
+            if(temperatura > temperaturaM) {
+                temperaturaM = temperatura;
+            }
+            
+            if(temperatura < temperaturaMini) {
+                temperaturaMini = temperatura;
+            }
+        }
+        
+        int temperaturaMedia = sumaTempe / 6;
+        
+        System.out.println("La temperatura media del día es: " + temperaturaMedia);
+        System.out.println("La temperatura más alta es: " + temperaturaM);
+        System.out.println("La temperatura más baja es: " + temperaturaMini);
+        
+    }
 }
