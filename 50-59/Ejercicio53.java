4ed4d76f7330f44a6a2e900b51770f5f486aa776
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:19:41 -0500

    Ejercicio53.java

--- 50-59/Ejercicio53.java
+++ 50-59/Ejercicio53.java
@@ -1,3 +1,11 @@
+import java.util.*;
 public class Ejercicio53 {
-    
+    public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite un lado del cuadrado");
+        double lado = entrada.nextDouble();
+        double area = (lado*lado)/2;
+        System.out.println("El area del cuadrado es "+area);
+
+    }
 }
