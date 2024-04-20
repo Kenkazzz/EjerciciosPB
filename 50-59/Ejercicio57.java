8db1989f9fd2fe7be08bb837c3803cf1b3209ce1
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:20:40 -0500

    Ejercicio57.java

--- 50-59/Ejercicio57.java
+++ 50-59/Ejercicio57.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
 public class Ejercicio57 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("Ingrese un número: ");
+        int numero = entrada.nextInt();
+        
+        int resultado = sumarDigitos(numero);
+        
+        System.out.println("La suma de los dígitos de " + numero + " es: " + resultado);
+    }
     
+    public static int sumarDigitos(int numero) {
+        int suma = 0;
+        
+        while(numero != 0) {
+            suma += numero % 10; // Sumar el último dígito
+            numero = numero / 10; // Eliminar el último dígito
+        }
+        
+        return suma;
+    }
 }
