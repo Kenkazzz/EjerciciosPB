e2d0078ee7abec7464ebc2cf7af249f5ac249de6
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:25:38 -0500

    Ejercicio65.java

--- 60-69/Ejercicio65.java
+++ 60-69/Ejercicio65.java
@@ -1,3 +1,19 @@
+import java.util.*;
+
 public class Ejercicio65 {
-    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Digita un numero");
+        int numero = entrada.nextInt();
+
+        int suma=0;
+
+        for(int i=1;i<=numero;i++){
+            suma =suma+i;
+
+        }
+        System.out.println("La suma de todos los numeros que lleva "+numero+" es igual a "+suma);
+
+    }
 }
