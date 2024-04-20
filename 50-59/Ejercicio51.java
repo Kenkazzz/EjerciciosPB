295d44aae3aceeeb97944732a3082335a59b6e85
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:12:19 -0500

    Ejercicio51.java

--- 50-59/Ejercicio51.java
+++ 50-59/Ejercicio51.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Ejercicio51 {
-    
+    public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Conoce el area de un rectangulo ");
+        System.out.println("");
+        System.out.println("Introduce la base del rectangulo");
+        float base = entrada.nextFloat();
+        System.out.println("Introcuce la altura del rectangulo");
+        float altura = entrada.nextFloat();
+
+        float area = (altura*base);
+        System.out.println("El area de el rectangulo es "+area);
+    }
 }
