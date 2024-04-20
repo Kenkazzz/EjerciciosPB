2ffcdd2a8817de92a6ffc1bccdd4321483a41b59
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:25:12 -0500

    Ejercicio63.java

--- 60-69/Ejercicio63.java
+++ 60-69/Ejercicio63.java
@@ -1,3 +1,26 @@
+import java.util.*;
+
 public class Ejercicio63 {
-    
+     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int suma = 0;
+        
+        System.out.println("Introduce valores enteros (0 para terminar):");
+        
+        while (true) {
+            int valor = entrada.nextInt();
+            
+            if (valor == 0) {
+                break;
+            } else if (valor > 0) {
+                suma += valor;
+            }
+            
+            if (valor > 20 && valor <= 30) {
+                break;
+            }
+        }
+        
+        System.out.println("La suma de los valores mayores a 0 es: " + suma);
+    }
 }
