e7a2a44ef651680e0e5944e76c665a1f2eebb84d
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:11:25 -0500

    Ejercicio49.java

--- 40-49/Ejercicio49.java
+++ 40-49/Ejercicio49.java
@@ -1,3 +1,16 @@
+import java.util.*;
 public class Ejercicio49 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa un texto: ");
+        String texto = scanner.nextLine();
+        
+        char primeraLetra = obtenerPrimeraLetra(texto);
+        
+        System.out.println("La primera letra de la cadena ingresada es: " + primeraLetra);
+    }
     
+    public static char obtenerPrimeraLetra(String texto) {
+        return texto.charAt(0);
+    }
 }
