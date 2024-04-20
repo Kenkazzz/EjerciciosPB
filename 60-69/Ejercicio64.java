d59213b778d06bbe2a86a34a6f7270d93c374da9
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:25:25 -0500

    Ejercicio64.java

--- 60-69/Ejercicio64.java
+++ 60-69/Ejercicio64.java
@@ -1,3 +1,22 @@
+import java.util.*;
+
 public class Ejercicio64 {
-    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Digita un numero");
+        int numero = entrada.nextInt();
+
+        System.out.println("Digite el numero al que desea potenciarlo");
+        int potencia = entrada.nextInt();
+
+        int potenciado=1;
+
+        for(int i=1;i<=potencia;i++){
+            potenciado = potenciado*numero;
+
+        }
+        System.out.println("El numero "+numero+" potenciado al "+potencia+" es igual a "+potenciado);
+
+    }
 }
