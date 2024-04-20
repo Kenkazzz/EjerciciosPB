2ca462be47826f531d2aac0cd27062d5681d3342
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:22:52 -0500

    Ejercicio59.java

--- 50-59/Ejercicio59.java
+++ 50-59/Ejercicio59.java
@@ -1,3 +1,16 @@
+import java.util.*;
 public class Ejercicio59 {
-    
+    public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite un numero del 1 al 10");
+        int num = entrada.nextInt();
+        if (num<0 || num>10) {
+            System.out.println("Solo permite numeros del 1 al 10");
+        }else{
+            for(int i=0; i<11; i++){
+                int multi = i*num;
+                System.out.println(num+" X "+i+" = "+multi);
+            }
+        }
+    }
 }
