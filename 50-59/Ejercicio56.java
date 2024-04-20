73e8fe121440eaa1fcec412255870abcccf83830
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:20:22 -0500

    Ejercicio56.java

--- 50-59/Ejercicio56.java
+++ 50-59/Ejercicio56.java
@@ -1,3 +1,13 @@
+import java.util.*;
 public class Ejercicio56 {
-    
+    public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite la cantidad de segundos");
+        double segundo = entrada.nextDouble();
+        double minutos = segundo/60;
+        double horas = segundo/3600;
+        System.out.println("la cantidad de segundos es "+segundo+" segundos");
+        System.out.println("La cantidad de minutos es "+minutos+" minutos");
+        System.out.println("La cantidad de horas es "+horas+" horas");
+    }
 }
