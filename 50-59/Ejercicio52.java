ec0d1d1a5f3ee0c822c7f3c811da276d6443e276
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:12:37 -0500

    Ejercicio52.java

--- 50-59/Ejercicio52.java
+++ 50-59/Ejercicio52.java
@@ -1,3 +1,12 @@
+import java.util.*;
 public class Ejercicio52 {
-    
+    public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingresa el numero que deseas conocer la tabla ");
+        int num = entrada.nextInt();
+        for(int i = 0; i<11;i++){
+            int tabla = i*num;
+            System.out.println(num+" X "+i+" = "+tabla);
+        }
+    }
 }
