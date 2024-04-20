386f75efa2475d3777e81d27a7bfb8416730b9b6
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:19:52 -0500

    Ejercicio54.java

--- 50-59/Ejercicio54.java
+++ 50-59/Ejercicio54.java
@@ -1,3 +1,10 @@
+import java.util.*;
 public class Ejercicio54 {
-    
+    public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite los centimetros");
+        double centi = entrada.nextDouble();
+        double pulgadas = centi/2.54;
+        System.out.println("En "+centi+" centimetros hay "+pulgadas+" pulgadas");
+    }
 }
