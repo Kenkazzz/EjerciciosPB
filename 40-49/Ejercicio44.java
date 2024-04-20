99dee6cafcc3a71fe298dc32ba475c04c963c18c
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:08:30 -0500

    Ejercicio44.java

--- 40-49/Ejercicio44.java
+++ 40-49/Ejercicio44.java
@@ -1,3 +1,17 @@
+import java.util.*;
 public class Ejercicio44 {
+            public static void main(String[] args) {
+            double pi = Math.PI;
     
+            for (int i = 2; i <= 10; i++) {
+                double potencia = calcularPotencia(pi, i);
+                double raizCuadrada = Math.sqrt(potencia);
+    
+                System.out.println("Potencia de PI a la " + i + ": " + potencia);
+                System.out.println("Raíz cuadrada de la potencia: " + raizCuadrada);
+            }
+}
+public static double calcularPotencia(double base, int exponente) {
+    return Math.pow(base, exponente);
+}
 }
