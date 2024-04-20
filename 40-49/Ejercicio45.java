6dfb6668e53d87715dc992bbe1ab475ea5fcc4b0
Kenkazzz <159142902+Kenkazzz@users.noreply.github.com>
2024-04-19 20:09:39 -0500

    Ejercicio45.java

--- 40-49/Ejercicio45.java
+++ 40-49/Ejercicio45.java
@@ -1,3 +1,16 @@
+import java.util.*;
 public class Ejercicio45 {
-    
+    public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digita un numero");
+        
+        double n1 = entrada.nextDouble();
+        if (n1<0) {
+            System.out.println("-1");
+        }else if(n1>0){
+            System.out.println("1");
+        }else{
+            System.out.println("0");
+        }
+    }
 }
